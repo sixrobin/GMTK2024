741e0953d10cac17d627257b370550e7794c3c2e
Maxence <maxence.delacroix@epitech.eu>
2024-08-17 00:37:07 +0200

    food, food manager, food spanwer, player collision. press space bar to try!

---
+++ scripts/food.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name Food
+
+func _ready() -> void:
+	FoodManager.register_food(self)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.eat(self)
+		
+func apply_eat(player: Player):
+	player.scale *= 1.1
+	destroy()
+
+func destroy():
+	FoodManager.unregister_food(self)
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
+class_name Player
 
+@export var SMOOTH_SPEED: float = 5
 
-func _ready():
-	print("Player ready!")
-	
-	
+var target_food: Node = null
+
 func _process(delta: float):
-	print("Player process!")
+	if target_food == null:
+		target_food = FoodManager.get_food()
+	if target_food != null:
+		self.position = lerp(self.position, target_food.position, delta * SMOOTH_SPEED)
+
+
+func eat(food: Food):
+	food.apply_eat(self)
