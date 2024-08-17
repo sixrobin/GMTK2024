81b8c1ae15da6286e66a71ef6b5e922096174bcf
VictorTer <victor.ternisien@gmail.com>
2024-08-17 21:48:23 +0200

    Added timer

---
+++ scripts/UI/timer.gd
@@ -0,0 +1,11 @@
+extends Control
+
+var time_elapsed: float = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	var minutes := time_elapsed / 60
+	var seconds := fmod(time_elapsed, 60)
+	var time_string := "%02d:%02d" % [minutes,seconds]
+	$Label.text = time_string
