0055a9b573494aea7ae0833f08410635bd869c07
VictorTer <victor.ternisien@gmail.com>
2024-08-17 10:43:54 +0200

    Stun feature

--- scripts/food/food.gd
+++ scripts/food/food.gd
@@ -1,15 +1,18 @@
 extends Node2D
 class_name Food
 
+@export var stun_duration: float = 0
+
 func _ready() -> void:
 	FoodManager.register_food(self)
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.eat(self)
 		
 func apply_eat(player: Player):
+	if stun_duration != 0:
+		player.stun(stun_duration)
 	print("super apply eat")
 	player.scale *= 1.1
 	destroy()
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,12 @@ class_name Player
 @export var SMOOTH_SPEED: float = 5
 
 var target_food: Node = null
+@export var stun_timer: Timer = null
+var is_stunned: bool = false
 
 func _process(delta: float):
+	if is_stunned:
+		return
 	if target_food == null:
 		target_food = FoodManager.get_food()
 	if target_food != null:
@@ -14,3 +18,12 @@ func _process(delta: float):
 
 func eat(food: Food):
 	food.apply_eat(self)
+
+func stun(duration: float):
+	if is_stunned:
+		return
+	is_stunned = true
+	stun_timer.start(duration)
+
+func _on_stun_timer_timeout() -> void:
+	is_stunned = false
