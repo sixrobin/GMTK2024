422f5d75e1e0a0852b9aaaf3051f1e034c929232
Maxence <maxence.delacroix@epitech.eu>
2024-08-18 10:29:46 +0200

    attraction handler

---
+++ scripts/objects/attraction_handler.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name AttractionHandler
+
+@export var priority: int = 10
+@export var timer: Timer = null
+
+var on_click_handler: ObjectOnClickHandler
+
+func set_on_click_handler(handler: ObjectOnClickHandler):
+	self.on_click_handler = handler
+	timer.timeout.connect(on_timer_reached)
+
+func attract():
+	self.on_click_handler.parent.set_attractive(true, priority)
+	timer.start()
+	
+func on_timer_reached():
+	self.on_click_handler.parent.set_attractive(false, priority)
+	timer.stop()
