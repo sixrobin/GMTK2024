816225b36819cc9cadd6e051d1e9085a177d97d7
VictorTer <victor.ternisien@gmail.com>
2024-08-18 14:47:25 +0200

    Changed hunger progress bar to texture progress bar

--- scripts/UI/hunger_gauge.gd
+++ scripts/UI/hunger_gauge.gd
@@ -4,10 +4,10 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	CreatureSingleton.creature.hunger_modified.connect(refresh)
-	$ProgressBar.value = CreatureSingleton.creature.max_hunger
+	$TextureProgressBar.value = CreatureSingleton.creature.max_hunger
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func refresh(old_hunger, new_hunger):
 	#print("hunger changed from %s to %s" % [old_hunger,new_hunger])
-	$ProgressBar.value = new_hunger
+	$TextureProgressBar.value = new_hunger
